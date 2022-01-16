50af0604ef8cc03e10bb4161915245898947923d
coval3nte <65908512+coval3nte@users.noreply.github.com>
2022-01-16 19:53:47 +0100

    update version

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sellix-python-sdk",
-    version="1.0",
+    version="1.0.1",
     packages=find_packages(),
     url='https://github.com/Sellix/python-sdk',
     install_requires=[
