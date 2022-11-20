18d57494dea43ff032fe88c0e8b5a25a1c50aa31
Daniele Servadei <servadei.d@gmail.com>
2022-11-20 18:10:38 +0100

    update version in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name="sellix-python-sdk",
-    version="1.0.2.1",
+    version="1.0.3",
     packages=find_packages(),
     url='https://github.com/Sellix/python-sdk',
     install_requires=[
